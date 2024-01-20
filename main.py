43025886b701d7c9703f86a22a5956b55ececd17
Gustavo Almada <gmda.snf23@uea.edu.br>
2024-01-20 10:41:57 -0400

    Organização do Código

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import sys
 
 pygame.init()
 
+# Screen
 WIDTH = 600
 HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-
-
+# Sprites
 player = pygame.image.load('assets/player (1).png')
-player= pygame.transform.scale(player, (150, 150))
+player = pygame.transform.scale(player, (150, 150))
 heart = pygame.image.load('assets/heart.png')
 heart = pygame.transform.scale(heart, (100, 50))
 x_sprite = 275
 y_sprite = 450
 
+# Colors
 GREY = (212, 218, 212)
 PURPLE = (153, 51, 153)
 
@@ -29,8 +30,7 @@ while True:
     clock = pygame.time.Clock().tick(10)
     screen.fill(0)
 
-
-
+    # Controls
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -43,6 +43,7 @@ while True:
     if keys[pygame.K_LEFT]:
         x_sprite -= 15
 
+    # Hud
     pygame.draw.line(screen, GREY, [0, 70], [0, HEIGHT], 10)
     pygame.draw.line(screen, GREY, [WIDTH, 70], [WIDTH, HEIGHT], 10)
 
@@ -71,6 +72,3 @@ while True:
         screen.blit(heart, (150, 0))
     else:
         screen.blit(heart, (100, 0))
-
-
-
